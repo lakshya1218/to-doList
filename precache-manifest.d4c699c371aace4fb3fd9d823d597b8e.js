a9238f887e9950a69c5dae111a6d4501e7de2db0
Lakshya Jain <jainlakshya1218@gmail.com>
2023-09-08 11:43:35 +0530

    Updates

---
+++ precache-manifest.d4c699c371aace4fb3fd9d823d597b8e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f918e0c841a84f33d3f4cbadfd88fdee",
+    "url": "/to-doList/index.html"
+  },
+  {
+    "revision": "2a8dc2ec8a7a247d5ffe",
+    "url": "/to-doList/static/js/2.4b3d7108.chunk.js"
+  },
+  {
+    "revision": "6910544bb47a2071f4c0",
+    "url": "/to-doList/static/js/main.d958b32f.chunk.js"
+  },
+  {
+    "revision": "a89d8086eccccb4a8601",
+    "url": "/to-doList/static/js/runtime-main.3a54d579.js"
+  }
+]);
